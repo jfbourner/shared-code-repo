0de2db5e02194551957322540b3537176030dfc8
jfbourner <jfbourner@gmail.com>
2026-05-20 14:21:07 +0100

    Add task executor code

--- Main.java
+++ Main.java
@@ -1,4 +1,40 @@
 public Main {
+    @Bean
+    public ThreadPoolTaskScheduler taskScheduler() {
+        return new ThreadPoolTaskScheduler() {
 
-    psvm
+            @Override
+            protected ExecutorService initializeExecutor(
+                    ThreadFactory threadFactory,
+                    RejectedExecutionHandler rejectedExecutionHandler) {
+
+                ScheduledThreadPoolExecutor executor =
+                        new ScheduledThreadPoolExecutor(getPoolSize(), threadFactory, rejectedExecutionHandler) {
+
+                            @Override
+                            public void shutdown() {
+                                log.warn("==> ScheduledThreadPoolExecutor.shutdown() called directly\n{}",
+                                        stackTrace());
+                                super.shutdown();
+                            }
+
+                            @Override
+                            public List<Runnable> shutdownNow() {
+                                log.warn("==> ScheduledThreadPoolExecutor.shutdownNow() called directly\n{}",
+                                        stackTrace());
+                                return super.shutdownNow();
+                            }
+
+                            private String stackTrace() {
+                                StringWriter sw = new StringWriter();
+                                new Exception("executor shutdown trace").printStackTrace(new PrintWriter(sw));
+                                return sw.toString();
+                            }
+                        };
+
+                return executor;
+            }
+        };
+    }
+
 }
